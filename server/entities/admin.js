ff7e095ccbea75dd1b61e1251440da62536a18e5
8086021001 <anoopa413@gmail.com>
2024-01-09 10:48:26 +0530

    First commit

---
+++ server/entities/admin.js
@@ -0,0 +1,12 @@
+
+
+
+// export default function user(username, password, email, role, createdAt) {
+//     return {
+//       getUserName: () => username,
+//       getPassword: () => password,
+//       getEmail: () => email,
+//       getRole: () => role,
+//       getCreatedAt: () => createdAt
+//     };
+//   }
