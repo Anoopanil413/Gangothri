f96c0715102238b8121c1bed236fe03913fb3a36
Trideep <118520595+trideepkumar@users.noreply.github.com>
2024-01-09 15:04:30 +0530

    Delete server/config.js directory

--- server/config.js/config.js
+++
@@ -1,8 +0,0 @@
-// basic url configuration of mongoose  port jwt
-export default {
-    port: process.env.PORT || 1234,
-    mongo: {
-      uri: process.env.MONGO_URL || 'mongodb://localhost:27017/Gangothri'
-    },
-    jwtSecret: process.env.JWT_SECRET 
-  };
