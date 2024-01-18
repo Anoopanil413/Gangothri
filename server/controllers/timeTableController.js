5668d0344478b904e1f4469037ee3dbfa74b58d6
8086021001 <anoopa413@gmail.com>
2024-01-18 09:47:56 +0530

    time table updated

--- server/controllers/timeTableController.js
+++ server/controllers/timeTableController.js
@@ -2,5 +2,11 @@
 
 
 export default function tableController(){
+
+    const addTable = ()=>{
+
+    }
+
     
+
 }
--- server/framework/database/mongoDB/models/timeTable.js
+++ server/framework/database/mongoDB/models/timeTable.js
@@ -6,18 +6,20 @@ const timeTable = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         require:true
     },
-    day_of_week: { type: String, required: true },
-    time_table: [{
-        subject_name: { type: String, required: true },
-        teacher_name: { type: String, required: true },
-        start_time: { type: String, required: true },
-        end_time: { type: String, required: true },
-      }],
+    week: [{
+        day_of_week: { type: String, required: true },
+        time_table: [{
+            subject_name: { type: String, required: true },
+            teacher_name: { type: String, required: true },
+            start_time: { type: String, required: true },
+            end_time: { type: String, required: true },
+        }],
+    }]
 
 })
 
 
-const TimeTable = mongoose.model('TimeTable', timeTableSchema);
+const TimeTable = mongoose.model('TimeTable', timeTable);
 export default TimeTable
 
 
